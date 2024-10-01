a3afd63449e92169792adc77a93c7c647217a3f1
DarlanPrado <darlandoprado2014@gmail.com>
2024-10-01 17:19:16 -0300

    :construction: desenvolvimento inicial dos controllers

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::prefix('api')->group(function () {
+    // Listar todos os contatos
+    Route::get('/contacts', [ContactController::class, 'index']);
+    
+    // // Obter um contato específico
+    // Route::get('/contacts/{id}', [ContactController::class, 'show']);
+    
+    // // Criar um novo contato
+    // Route::post('/contacts', [ContactController::class, 'store']);
+    
+    // // Atualizar um contato existente
+    // Route::put('/contacts/{id}', [ContactController::class, 'update']);
+    
+    // // Deletar um contato
+    // Route::delete('/contacts/{id}', [ContactController::class, 'destroy']);
 });
